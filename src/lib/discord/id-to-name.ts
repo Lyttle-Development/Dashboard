774ab5c2a5e23e13052eefa8be477c084a00df17
stualyttle <stualyttle@users.noreply.github.com>
2025-02-18 19:06:47 +0100

    Update names

--- src/lib/discord/id-to-name.ts
+++ src/lib/discord/id-to-name.ts
@@ -1,8 +1,8 @@
 export function idToName(id: string) {
   const users = {
-    "132487290835435521": "Kilian",
-    "643450818690940929": "Emma",
-    "469271199143428099": "Luda",
+    "132487290835435521": "Kilian De Bock",
+    "643450818690940929": "Emma Van der Haeghen",
+    "469271199143428099": "Liudmila Mikhaylinova",
   };
 
   return users[id] || "Unknown";
