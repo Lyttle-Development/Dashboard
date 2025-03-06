fc819753c3124f74eb62c9948afb2b88dce5f7be
stualyttle <stualyttle@users.noreply.github.com>
2025-03-06 11:05:01 +0100

    Categorise Main Navigation

--- src/components/MainNav/index.tsx
+++ src/components/MainNav/index.tsx
@@ -6,43 +6,67 @@ import {
   faCircleUser,
   faDiagramProject,
   faFileInvoiceDollar,
+  faHandHoldingDollar,
+  faHouseChimney,
   faListCheck,
   faPrint,
   faTag,
 } from "@fortawesome/free-solid-svg-icons";
+import { useApp } from "@/contexts/App.context";
 
 export interface MainNavProps {}
 
 export function MainNav({}: MainNavProps) {
+  const app = useApp();
   return (
-    <aside className={`${styles["main-menu"]}`}>
+    <aside className={styles.main_menu}>
       <Logo />
       <nav>
+        <h5>Operations</h5>
         <ul>
+          <MainNavItem href={"/"}>
+            <Icon icon={faHouseChimney} />
+            Home
+          </MainNavItem>
           <MainNavItem href={"/project"}>
             <Icon icon={faDiagramProject} />
             Projects
           </MainNavItem>
-          <MainNavItem href={"/print"}>
-            <Icon icon={faPrint} />
-            Printing
+          {app.isAdmin && (
+            <MainNavItem href={"/print"}>
+              <Icon icon={faPrint} />
+              Printing
+            </MainNavItem>
+          )}
+          <MainNavItem href={"/task"}>
+            <Icon icon={faListCheck} />
+            Tasks
           </MainNavItem>
+        </ul>
+      </nav>
+      <nav>
+        <h5>Administration</h5>
+        <ul>
           <MainNavItem href={"/customer"}>
             <Icon icon={faCircleUser} />
             Customers
-          </MainNavItem>
-          <MainNavItem href={"/invoice"}>
-            <Icon icon={faFileInvoiceDollar} />
-            Invoices
           </MainNavItem>
           <MainNavItem href={"/price"}>
             <Icon icon={faTag} />
             Prices
           </MainNavItem>
-          <MainNavItem href={"/task"}>
-            <Icon icon={faListCheck} />
-            Tasks
-          </MainNavItem>
+          {app.isManager && (
+            <>
+              <MainNavItem href={"/invoice"}>
+                <Icon icon={faFileInvoiceDollar} />
+                Invoices
+              </MainNavItem>
+              <MainNavItem href={"/expense"}>
+                <Icon icon={faHandHoldingDollar} />
+                Expenses
+              </MainNavItem>
+            </>
+          )}
         </ul>
       </nav>
       <ul className={styles.sub_menu}>
