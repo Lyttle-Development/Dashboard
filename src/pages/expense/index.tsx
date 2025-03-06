f62921d9b6ab8783f9ee69f8ffa0397460a700ab
stualyttle <stualyttle@users.noreply.github.com>
2025-03-06 11:53:53 +0100

    Add expenses home

---
+++ src/pages/expense/index.tsx
@@ -0,0 +1,71 @@
+import { Layout } from "@/layouts";
+import { Container } from "@/components/Container";
+import { Icon } from "@/components/Icon";
+import {
+  faHandHoldingDollar,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+import { Select } from "@/components/Select";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import { Loader } from "@/components/Loader";
+import { Button } from "@/components/Button";
+import { fetchApi } from "@/lib/fetchApi";
+import { Expense } from "@/lib/prisma";
+import { SideToSide } from "@/components/SideToSide";
+
+export function Page() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  const fetchExpenses = useCallback(async () => {
+    setLoading(true);
+    const expensesData = await fetchApi<Expense[]>({
+      table: "expense",
+      where: {
+        statusId: {
+          not: "2dee2fe0-e126-4ac4-b451-8e75c3316c7b", // Closed
+        },
+      },
+    });
+    setExpenses(expensesData);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    void fetchExpenses();
+  }, [fetchExpenses]);
+
+  if (loading) return <Loader />;
+
+  return (
+    <Container>
+      <h1>Expenses</h1>
+
+      <SideToSide>
+        <Select
+          label="Select Expense"
+          icon={faMagnifyingGlass}
+          options={
+            expenses && expenses.length
+              ? expenses.map((expense) => ({
+                  label: expense.name,
+                  value: expense.id,
+                }))
+              : []
+          }
+          onValueChange={(expenseId) => router.push(`/expense/${expenseId}`)}
+          disabled={!(expenses && expenses.length)}
+        />
+        <Button href="/expense/create">
+          <Icon icon={faHandHoldingDollar}>Create Expense</Icon>
+        </Button>
+      </SideToSide>
+    </Container>
+  );
+}
+
+Page.getLayout = Layout.getDefault;
+
+export default Page;
