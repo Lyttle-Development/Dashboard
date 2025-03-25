ffbec1a31477e22ed9fc44fdce51b6da53e58981
stualyttle <stualyttle@users.noreply.github.com>
2025-03-25 16:48:55 +0100

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,27 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import { SITE_NAME } from "@/constants";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet="utf-8" />
+        <title>{SITE_NAME}</title>
+
+        {/* <!-- Favicon for browsers that support SVG format --> */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        {/* <!-- Favicon for browsers that support PNG format --> */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        {/* <!-- Favicon for older browsers or those that support ICO format --> */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* <!-- For Apple devices --> */}
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        {/* <!-- For Android devices (optional) --> */}
+        <link rel="icon" sizes="192x192" href="/favicon.png" />
+        {/* <!-- For Android devices (recommended) --> */}
         <meta name="theme-color" content="#ffffff" />
+
+        {/* <!-- Basic styles --> */}
         <style>
           {`
             body {
