4e36b4f4e765b8153aec92903c515fd657e55bff
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2025-04-23 09:52:28 +0200

    Add check

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,31 @@
-import type { ReactElement, ReactNode } from "react";
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
-import "./../styles/reset.scss";
-import "./../styles/defaults.scss";
-import "@radix-ui/themes/styles.css";
-import "./../styles/global.scss";
-import { SessionProvider } from "next-auth/react";
-import { AppProvider } from "@/contexts/App.provider";
+import type {ReactElement, ReactNode} from 'react';
+import type {NextPage} from 'next';
+import type {AppProps} from 'next/app';
+import './../styles/reset.scss';
+import './../styles/defaults.scss';
+import '@radix-ui/themes/styles.css';
+import './../styles/global.scss';
+import {SessionProvider} from 'next-auth/react';
+import {AppProvider} from '@/contexts/App.provider';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+    getLayout?: (page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+    Component: NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
-  const skipAuth = Component.skipAuth ?? false
+export default function App({Component, pageProps}: AppPropsWithLayout) {
+    // Use the layout defined at the page level, if available
+    const getLayout = Component?.getLayout ?? ((page) => page);
+    const skipAuth = Component?.skipAuth ?? false;
 
-  return (
-    <SessionProvider>
-      <AppProvider skipAuth={skipAuth}>{getLayout(<Component {...pageProps} />)}</AppProvider>
-    </SessionProvider>
-  );
+    return (
+        <SessionProvider>
+            <AppProvider skipAuth={skipAuth}>{getLayout(
+                <Component {...pageProps} />)}</AppProvider>
+        </SessionProvider>
+    );
 }
